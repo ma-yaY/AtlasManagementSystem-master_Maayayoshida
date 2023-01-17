071dbd1055e6729a77981dd44efcbdb2b557bcbc
Maaya <tommy6.loveandpop@gmail.com>
2023-01-17 12:02:51 +0900

    初期データの登録の完了

--- database/seeds/SubjectsTableSeeder.php
+++ database/seeds/SubjectsTableSeeder.php
@@ -12,10 +12,10 @@ class SubjectsTableSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('subjects')->insert([
-            'Japanese' => '1',
-            'Math' => '2',
-            'English' => '3',
+        DB::table('Subjects')->insert([
+            'subject' => '国語',
+            'subject' => '数学',
+            'subject' => '英語'
         ]);
         // 国語、数学、英語を追加
     }
