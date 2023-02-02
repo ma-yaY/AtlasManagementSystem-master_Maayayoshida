67e62805af4ed5ea307bd079d5183c5538efdb85
Maaya <tommy6.loveandpop@gmail.com>
2023-02-02 21:59:09 +0900

    サイドバーの項目の一部を管理者（role1のユーザー）のみに表示させた

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -22,7 +22,7 @@
         <p><a href="{{ route('top.show') }}">トップ</a></p>
         <p><a href="/logout">ログアウト</a></p>
         <p><a href="{{ route('calendar.general.show',['user_id' => Auth::id()]) }}">スクール予約</a></p>
-        @if(auth()->user()->admin_role === 1)
+        @if(auth()->user()->role === 1)
           <p><a href="{{ route('calendar.admin.show',['user_id' => Auth::id()]) }}">スクール予約確認</a></p>
           <p><a href="{{ route('calendar.admin.setting',['user_id' => Auth::id()]) }}">スクール枠登録</a></p>
         @endif
