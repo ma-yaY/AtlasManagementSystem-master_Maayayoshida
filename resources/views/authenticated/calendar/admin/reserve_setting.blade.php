953088411c1a4db753e1c0ea202f608d6e699989
Maaya <tommy6.loveandpop@gmail.com>
2023-06-20 17:02:15 +0900

    no message

--- resources/views/authenticated/calendar/admin/reserve_setting.blade.php
+++ resources/views/authenticated/calendar/admin/reserve_setting.blade.php
@@ -4,8 +4,8 @@
   <div class="resarve_settings" >
     <p class="text-center">{{ $calendar->getTitle() }}</p>
     {!! $calendar->render() !!}
-    <div class="adjust-table-btn m-auto text-right">
-      <input type="submit" class="btn btn-primary" value="登録" form="reserveSetting" onclick="return confirm('登録してよろしいですか？')">
+    <div class="text-right">
+      <input type="submit" class="btn btn-primary " value="登録" form="reserveSetting" onclick="return confirm('登録してよろしいですか？')">
     </div>
   </div>
 </div>
